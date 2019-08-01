ed38d884e554056ff4a1922eab1ef416ca18c812
ItsDiglett <53492062+ItsDiglett@users.noreply.github.com>
2019-08-01 04:13:40 -0400

    Add files via upload

---
+++ cogs/methode.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from config import token
+from config import place
+import random
+import ffmpeg
+import asyncio
+import os
+
+class Example(commands.Cog):
+    def __iniit__(self,client):
+        self.client=client
+        
+    @commands.command()
+    async def methode(self, ctx):                
+        channel = ctx.message.author.voice.channel
+        server = ctx.message.guild.voice_client
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio((place) + '/' + 'methode.mp3'), after=lambda e: print('done', e))
+        await asyncio.sleep(0.1)
+        while vc.is_playing():
+                await asyncio.sleep(0.1)
+        vc.stop()
+        await vc.disconnect()  
+
+    @commands.command()
+    async def methode2(self, ctx):
+        channel = ctx.message.author.voice.channel
+        server = ctx.message.guild.voice_client
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio((place) + '/' + 'methode.wav'))
+        await asyncio.sleep(0.1)
+        while vc.is_playing():
+                await asyncio.sleep(0.1)
+        vc.stop()
+        await vc.disconnect()
+
+    @commands.command(pass_context=True)
+    async def methode3(self, ctx):
+            if ctx.guild.id == (413059339138629632):
+                    channel = ctx.message.author.voice.channel
+                    server = ctx.message.guild.voice_client
+                    vc = await channel.connect()
+                    vc.play(discord.FFmpegPCMAudio((place) + '/' + 'methode3.wav'), after=lambda e: print('done', e))
+                    await asyncio.sleep(0.1)
+                    while vc.is_playing():
+                            await asyncio.sleep(0.1)
+                    vc.stop()
+                    await vc.disconnect()
+
+            else:
+                    await ctx.send("You can't use that.")
+def setup(client):
+        client.add_cog(Example(client))
