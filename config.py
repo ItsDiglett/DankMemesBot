75c18770e78c106daf21638cc226295b8c0e6cc8
ItsDiglett <53492062+ItsDiglett@users.noreply.github.com>
2019-08-05 04:50:44 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#This is for DankMemesBot
+token = 'NTgzNzM3ODU4NTMyMDQ4ODk3.XUCPnA.S9ixscjEpvdD062pCjKTzhHERxU'
+#This is for KeemBot
+token2 = 'NjA2MjQ5NjMzNzczMjU2NzIw.XUeTtw.E6W9r-j0e1rUgHamKVZjukMesV4'
+#This is a bannedusers list for KeemBot/DankMemesBot
+bannedusers = ()
+
+
